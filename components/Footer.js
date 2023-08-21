86e9d991e70530d3115c4950ee9734545f0be127
MelakuDemeke <melaku.demeke789@gmail.com>
2023-08-21 16:29:34 +0300

    footer componect added

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+	return (
+			<footer className="bg-white dark:bg-gray-900 border-t-2 border-gray-500 dark:border-blue-600 ">
+				<p className="flex items-center justify-center">CVComposer</p>
+			</footer>
+			);
+}
+
+export default Footer;
