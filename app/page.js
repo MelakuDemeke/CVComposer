d2cc749d48479ba15ddc4e63d45dbbd6c85f37dd
MelakuDemeke <melaku.demeke789@gmail.com>
2023-08-22 10:14:12 +0300

    remove unwanted class

--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
+import Hero from '@/components/Hero'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen">
+      <Hero />
     </main>
   )
 }
