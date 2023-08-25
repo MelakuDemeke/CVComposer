2442f61edc6d706d511cbeaf4b01b471e39f4646
MelakuDemeke <melaku.demeke789@gmail.com>
2023-08-25 16:56:37 +0300

    preview classified into left and right

--- components/Preview/Preview.js
+++ components/Preview/Preview.js
@@ -1,7 +1,12 @@
 const Preview = () => {
-	return(
-		<div>
-			<p>This is preview</p>
+	return (
+		<div className="w-full h-full flex items-start justify-start overflow-auto">
+			<div className="w-2/6 h-auto min-h-full p-6">
+
+			</div>
+			<div className="w-4/6 h-auto min-h-full">
+
+			</div>
 		</div>
 	)
 }
