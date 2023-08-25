8aaa0bc7b1dec093ee8e3d5e9cbfb0f119d8f322
MelakuDemeke <melaku.demeke789@gmail.com>
2023-08-25 11:44:02 +0300

    update footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,14 @@
 const Footer = () => {
 	return (
-			<footer className="bg-white dark:bg-gray-900 border-t-2 border-gray-500 dark:border-blue-600 ">
-				<p className="flex items-center justify-center">CVComposer</p>
-			</footer>
-			);
+		<footer className="w-full max-w-[85rem] py-10 px-4 sm:px-6 lg:px-8 mx-auto border-t-2 border-gray-300 dark:border-gray-600">
+			<div class="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-6 mb-10">
+				<div class="col-span-full hidden lg:col-span-1 lg:block">
+					<a class="flex-none text-xl font-semibold dark:text-white" href="#" aria-label="Brand">CVComposer</a>
+					<p class="mt-3 text-xs sm:text-sm text-gray-600 dark:text-gray-400">© 2023 ALX</p>
+				</div>
+			</div>
+		</footer>
+	);
 }
 
 export default Footer;
