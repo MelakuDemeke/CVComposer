8092e2bba706d0e19dbf9b8df5cf66657ee403b8
MelakuDemeke <melaku.demeke789@gmail.com>
2023-08-25 16:56:17 +0300

    remove dummay data

--- components/Form/Form.js
+++ components/Form/Form.js
@@ -1,7 +1,7 @@
 const Form = () => {
 	return (
 		<div className="bg-slate-100 dark:bg-gray-900 w-full lg:w-2/5 h-full flex flex-col items-start justify-start overflow-auto p-5 xl:p-10">
-			
+
 		</div>
 	)
 }
