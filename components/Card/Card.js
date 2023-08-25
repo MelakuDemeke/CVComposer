38f86001b9aee3471770c0e48c7bddfecdde1dff
MelakuDemeke <melaku.demeke789@gmail.com>
2023-08-25 17:21:47 +0300

    card color added

---
+++ components/Card/Card.js
@@ -0,0 +1,10 @@
+const Card = () => {
+	return (
+		<div className="w-full mb-5 flex flex-col bg-sky-700 px-2 py-4 shadow-lg shadow-black">
+			<h4>Title</h4>
+		
+		</div>
+	)
+}
+
+export default Card;
---
+++ components/Form/FormPersonal.js
@@ -0,0 +1,11 @@
+import Card from "../Card/Card";
+
+const FormPersonal = () => {
+	return (
+		<Card>
+			
+		</Card>
+	)
+}
+
+export default FormPersonal;
