56326f901ea81ad3c7e26aa58fbf1e3400b31414
MelakuDemeke <melaku.demeke789@gmail.com>
2023-08-28 10:38:45 +0300

    render recived props

--- components/Card/Card.js
+++ components/Card/Card.js
@@ -1,8 +1,8 @@
-const Card = () => {
+const Card = ({ title, children }) => {
 	return (
-		<div className="w-full mb-5 flex flex-col bg-sky-700 px-2 py-4 shadow-lg shadow-black">
-			<h4>Title</h4>
-		
+		<div className="w-full mb-5 flex flex-col bg-gray-500 px-2 py-4 shadow-lg shadow-black">
+			<h4>{title}</h4>
+			{children}
 		</div>
 	)
 }
