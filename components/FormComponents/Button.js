0ca8110530ea565dc3e86e8fb1b2291b15cd9d03
MelakuDemeke <melaku.demeke789@gmail.com>
2023-08-28 10:55:23 +0300

    design for button

---
+++ components/FormComponents/Button.js
@@ -0,0 +1,23 @@
+export default function Button({
+	children,
+	loading = false,
+	variant = "default",
+	...props
+}) {
+	return (
+		<button
+			disabled={loading}
+			className={classNames({
+				"h-8 ml-2 mt-2 px-5 text-center text-white border-2 border-white text-base flex items-center justify-center cursor-pointer disabled:cursor-not-allowed disabled:bg-opacity-50": true,
+				"bg-transparent": variant === "default",
+				"bg-green-700": variant === "success",
+				"bg-red-700": variant === "danger",
+				"bg-orange-700": variant === "warning",
+				"bg-blue-400": variant === "info",
+			})}
+			{...props}
+		>
+			{loading ? "...." : children}
+		</button>
+	);
+}
