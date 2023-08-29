da093a01b9b94acf0d810d380a91df32a3e5af21
MelakuDemeke <melaku.demeke789@gmail.com>
2023-08-29 16:26:57 +0300

    text and background style added for left and right

--- components/Preview/Preview.js
+++ components/Preview/Preview.js
@@ -1,14 +1,21 @@
 "use client";
 import PreviewHeader from "./PreviewHeader";
+import { useDispatch, useSelector } from "react-redux";
 import PreviewPersonal from "./PreviewPersonal";
+import { setLeftSide, setRightSide } from "@/app/GlobalRedux/site";
 
 const Preview = () => {
+	const dispatch = useDispatch();
+	const { leftSide, rightSide, colors, isContentEditable } = useSelector(
+		(state) => state.site
+	);
+
 	return (
 		<div className="w-full h-full flex items-start justify-start overflow-auto">
-			<div className="w-2/6 h-auto min-h-full p-6">
+			<div style={{ background: colors.secondary, color: colors.text1 }} className="w-2/6 h-auto min-h-full p-6">
 				<PreviewPersonal />
 			</div>
-			<div className="w-4/6 h-auto min-h-full">
+			<div style={{ color: colors.text2 }} className="w-4/6 h-auto min-h-full">
 				<PreviewHeader />
 			</div>
 		</div>
