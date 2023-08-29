ea401222e4756e396f300c7824a1f66bb966283c
MelakuDemeke <melaku.demeke789@gmail.com>
2023-08-29 16:33:30 +0300

    give the card round button

--- components/Card/Card.js
+++ components/Card/Card.js
@@ -1,6 +1,6 @@
 const Card = ({ title, children }) => {
 	return (
-		<div className="w-full mb-5 flex flex-col bg-gray-500 px-2 py-4 shadow-lg shadow-black">
+		<div className="rounded-lg w-full mb-5 flex flex-col bg-gray-500 px-2 py-4 shadow-lg shadow-black">
 			<h4>{title}</h4>
 			{children}
 		</div>
