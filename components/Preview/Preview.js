38e0d6257198c16b0fc391ae7da34d1b26d81ec0
MelakuDemeke <melaku.demeke789@gmail.com>
2023-08-29 15:53:34 +0300

    personal render added

--- components/Preview/Preview.js
+++ components/Preview/Preview.js
@@ -1,8 +1,11 @@
+"use client";
+import PreviewPersonal from "./PreviewPersonal";
+
 const Preview = () => {
 	return (
 		<div className="w-full h-full flex items-start justify-start overflow-auto">
 			<div className="w-2/6 h-auto min-h-full p-6">
-
+				<PreviewPersonal />
 			</div>
 			<div className="w-4/6 h-auto min-h-full">
 
