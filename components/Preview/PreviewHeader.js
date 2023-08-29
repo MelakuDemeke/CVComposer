0fa0f0fc8e2b974ad608ae6a2033b942f34e781d
MelakuDemeke <melaku.demeke789@gmail.com>
2023-08-29 16:14:11 +0300

    title and description renderd

--- components/Preview/PreviewHeader.js
+++ components/Preview/PreviewHeader.js
@@ -6,6 +6,8 @@ const PreviewHeader = () => {
 	return (
 		<div className="flex flex-col items-center justify-center min-h-[200px]">
 			<h1 className="text-6xl text-center font-bold tracking-widest">{nameSurname}</h1>
+			<h2 className="mt-2 text-2xl text-center tracking-wider">{title}</h2>
+			{description && <div className="mt-2 text-2xl text-center tracking-wider">{description}</div>}
 		</div>
 	)
 }
