11f37009b37b26c9a346f393968f039cba0fe20c
MelakuDemeke <melaku.demeke789@gmail.com>
2023-08-29 16:17:53 +0300

    text style updated

--- components/Preview/PreviewHeader.js
+++ components/Preview/PreviewHeader.js
@@ -7,7 +7,7 @@ const PreviewHeader = () => {
 		<div className="flex flex-col items-center justify-center min-h-[200px]">
 			<h1 className="text-6xl text-center font-bold tracking-widest">{nameSurname}</h1>
 			<h2 className="mt-2 text-2xl text-center tracking-wider">{title}</h2>
-			{description && <div className="mt-2 text-2xl text-center tracking-wider">{description}</div>}
+			{description && <div className="w-full px-6 mt-6 break-words">{description}</div>}
 		</div>
 	)
 }
