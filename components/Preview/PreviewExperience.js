0d99435d1637337e7a059705c338fa3aeb4aee41
MelakuDemeke <melaku.demeke789@gmail.com>
2023-08-30 17:03:00 +0300

    preview exp styles and layout done

--- components/Preview/PreviewExperience.js
+++ components/Preview/PreviewExperience.js
@@ -3,10 +3,43 @@ import { useWords } from "@/hooks";
 import { useSelector } from "react-redux";
 
 const PreviewExperience = () => {
-	
-	return (
-		<div>
+	const { experience } = useSelector((state) => state.form);
+  const { colors } = useSelector((state) => state.site);
+  const words = useWords();
 
+  let items;
+
+  if (experience.length !== 0) {
+    items = React.Children.toArray(
+      experience.map((item) => (
+        <div className="flex flex-col mb-4">
+          <div className="flex flex-wrap justify-between">
+            <strong>{item.position}</strong>
+            <span>
+              {item.from} - {item.to}
+            </span>
+          </div>
+          <div>
+            {item.company}, {item.city}
+          </div>
+          <div className="text-sm">{item.description}</div>
+        </div>
+      ))
+    );
+  }
+	return (
+		<div className="flex flex-col w-full px-6 mt-2">
+			{experience.length !== 0 && (
+        <>
+          <div
+            className="p-2 mb-4 mt-4 font-bold text-center tracking-widest"
+            style={{ background: colors.primary, color: colors.titles }}
+          >
+            {words.experience_upper}
+          </div>
+          {items}
+        </>
+      )}
 		</div>
 	)
 }
