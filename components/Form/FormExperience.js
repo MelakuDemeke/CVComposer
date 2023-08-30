0cb8ebe5ab8d4d96f1dbcc7e97d90d78ec4881c0
MelakuDemeke <melaku.demeke789@gmail.com>
2023-08-30 16:53:57 +0300

    import necessary components

--- components/Form/FormExperience.js
+++ components/Form/FormExperience.js
@@ -1,4 +1,12 @@
 const { default: Card } = require("../Card/Card")
+import { Formik, Form } from "formik";
+import { Input, Textarea } from "../FormComponents";
+import { nanoid } from "nanoid";
+import { useDispatch, useSelector } from "react-redux";
+import { setExperience } from "@/app/GlobalRedux/form";
+import CardDetail from "../FormComponents/CardDetail";
+import { Buttons } from "../FormComponents/Buttons";
+import { useWords } from "@/hooks";
 
 const FormExperience = ({ handleResetData }) => {
 	return (
