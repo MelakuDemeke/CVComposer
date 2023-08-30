3ff7a8e5dbbcc559cfcfdf42a7c2b043d5439d4c
MelakuDemeke <melaku.demeke789@gmail.com>
2023-08-30 15:43:25 +0300

    hobbies preview add styles

--- components/Preview/PreviewHobbies.js
+++ components/Preview/PreviewHobbies.js
@@ -8,9 +8,31 @@ const PreviewHobbies = () => {
   const { colors } = useSelector((state) => state.site);
   const words = useWords();
 
+	let items;
+
+  if (hobbies.length !== 0) {
+    items = React.Children.toArray(
+      hobbies.map((item) => (
+        <div className="flex">
+          <strong>{item.name}</strong>
+        </div>
+      ))
+    );
+  }
+
 	return (
 		<div className='flex flex-col w-full'>
-
+			{hobbies.length !== 0 && (
+        <>
+          <div
+            className="p-2 mb-4 mt-4 font-bold text-center tracking-widest"
+            style={{ background: colors.primary, color: colors.titles }}
+          >
+            {words.hobbies_upper}
+          </div>
+          {items}
+        </>
+      )}
 		</div>
 	)
 }
