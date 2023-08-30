0f33a7064f95e1819fbe18dbb14feca95da2380d
MelakuDemeke <melaku.demeke789@gmail.com>
2023-08-30 16:16:49 +0300

    import necessarey components

--- components/Form/FormEducation.js
+++ components/Form/FormEducation.js
@@ -1,9 +1,17 @@
 import Card from "../Card/Card"
+import { Formik, Form } from "formik";
+import { Input } from "../FormComponents";
+import { useDispatch, useSelector } from "react-redux";
+import { setEducation } from "@/app/GlobalRedux/form";
+import { nanoid } from "nanoid";
+import CardDetail from "../FormComponents/CardDetail";
+import { Buttons } from "../FormComponents/Buttons";
+import { useWords } from "@/hooks";
 
 const FormEducation = () => {
 	return (
 		<Card>
-			
+
 		</Card>
     )
 }
