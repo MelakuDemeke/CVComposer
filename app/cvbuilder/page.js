056b97f3c40039129016adf7c0abb625330cfef6
MelakuDemeke <melaku.demeke789@gmail.com>
2023-08-30 20:56:54 +0300

    doc style fix

--- app/cvbuilder/page.js
+++ app/cvbuilder/page.js
@@ -9,19 +9,19 @@ export default function Home() {
 	const { language } = useSelector((state) => state.language);
 
 	const printRef = useRef();
-  const handlePrint = useReactToPrint({
-    content: () => printRef.current,
-  });
+	const handlePrint = useReactToPrint({
+		content: () => printRef.current,
+	});
 
 	useEffect(() => {
-    document.documentElement.lang = language;
-  }, [language]);
+		document.documentElement.lang = language;
+	}, [language]);
 
 	return (
 		<main className="min-h-screen h-auto lg:h-screen flex flex-wrap overflow-hidden">
-			<Form  handlePrint={handlePrint}/>
+			<Form handlePrint={handlePrint} />
 			<div className='w-full lg:w-3/5'>
-				<Preview ref={printRef}/>
+				<Preview ref={printRef} />
 			</div>
 		</main>
 	)
