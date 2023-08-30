8b1ef2b315ae9257cdf36af5edd7bdf4cbe7dcc8
MelakuDemeke <melaku.demeke789@gmail.com>
2023-08-30 15:13:29 +0300

    languge preview added

--- components/Preview/PreviewLanguage.js
+++ components/Preview/PreviewLanguage.js
@@ -7,9 +7,30 @@ const PreviewLanguage = () => {
   const { colors } = useSelector((state) => state.site);
   const words = useWords();
 
+	let items;
+
+	if (languages.length !== 0) {
+    items = React.Children.toArray(
+      languages.map((item) => (
+        <div className="flex justify-between">
+          <strong>{item.name}</strong> {item.level}
+        </div>
+      ))
+    );
+  }
 	return (
 		<div className="flex flex-col w-full">
-
+			{languages.length !== 0 && (
+        <>
+          <div
+            className="p-2 mb-4 mt-4 font-bold text-center tracking-widest"
+            style={{ background: colors.primary, color: colors.titles }}
+          >
+            {words.languages_upper}
+          </div>
+          {items}
+        </>
+      )}
 		</div>
 	)
 }
