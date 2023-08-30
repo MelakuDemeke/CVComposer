ca68827029a9751d2f87d37ebeada217aa870df9
MelakuDemeke <melaku.demeke789@gmail.com>
2023-08-30 17:18:47 +0300

    remove unused import

--- app/cvbuilder/page.js
+++ app/cvbuilder/page.js
@@ -1,9 +1,6 @@
-import Features from '@/components/Features'
+
 import Form from '@/components/Form/Form'
-import Hero from '@/components/Hero'
 import Preview from '@/components/Preview/Preview'
-import Stats from '@/components/Stats'
-import Testimonials from '@/components/Testimonials'
 
 export default function Home() {
 	return (
