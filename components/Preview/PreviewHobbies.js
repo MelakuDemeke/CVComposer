1ce1f2c3f112ff2af11af9ef8b589a6ffe23f04f
MelakuDemeke <melaku.demeke789@gmail.com>
2023-08-30 15:41:10 +0300

    import components and hookes

--- components/Preview/PreviewHobbies.js
+++ components/Preview/PreviewHobbies.js
@@ -1,7 +1,15 @@
+import React from 'react'
+import { useWords } from '@/hooks'
+import { useSelector } from "react-redux"
+
 
 const PreviewHobbies = () => {
+	const { hobbies } = useSelector((state) => state.form);
+  const { colors } = useSelector((state) => state.site);
+  const words = useWords();
+
 	return (
-		<div>
+		<div className='flex flex-col w-full'>
 
 		</div>
 	)
