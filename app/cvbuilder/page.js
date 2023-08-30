6ee49875df105220628938603b8d4e4820608e8e
MelakuDemeke <melaku.demeke789@gmail.com>
2023-08-30 17:26:01 +0300

    print handling added

--- app/cvbuilder/page.js
+++ app/cvbuilder/page.js
@@ -7,9 +7,19 @@ import { useSelector } from 'react-redux';
 
 export default function Home() {
 	const { language } = useSelector((state) => state.language);
+
+	const printRef = useRef();
+  const handlePrint = useReactToPrint({
+    content: () => printRef.current,
+  });
+
+	useEffect(() => {
+    document.documentElement.lang = language;
+  }, [language]);
+
 	return (
 		<main className="min-h-screen h-auto lg:h-screen flex flex-wrap overflow-hidden">
-			<Form />
+			<Form  handlePrint={handlePrint}/>
 			<div className='w-full lg:w-3/5'>
 				<Preview />
 			</div>
