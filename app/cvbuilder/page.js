6cd1a0dd3f31b6524f9779c41456895fc51be29c
MelakuDemeke <melaku.demeke789@gmail.com>
2023-08-30 17:23:41 +0300

    languge select state added

--- app/cvbuilder/page.js
+++ app/cvbuilder/page.js
@@ -2,8 +2,11 @@
 import Form from '@/components/Form/Form'
 import Preview from '@/components/Preview/Preview'
 import { useEffect, useRef } from 'react'
+import { useReactToPrint } from 'react-to-print';
+import { useSelector } from 'react-redux';
 
 export default function Home() {
+	const { language } = useSelector((state) => state.language);
 	return (
 		<main className="min-h-screen h-auto lg:h-screen flex flex-wrap overflow-hidden">
 			<Form />
