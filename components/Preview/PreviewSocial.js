6abd6c0bc9dea65e03b9d1f1aaa26149369e6915
MelakuDemeke <melaku.demeke789@gmail.com>
2023-08-30 10:31:38 +0300

    add prefix url to the social

--- components/Preview/PreviewSocial.js
+++ components/Preview/PreviewSocial.js
@@ -4,17 +4,51 @@ import { useWords } from "@/hooks";
 import { use } from "react";
 
 const PreviewSocial = () => {
-	const {social} = useSelector((state) => state.form);
-	const {colors} = useSelector((state) => state.site);
+	const { social } = useSelector((state) => state.form);
+	const { colors } = useSelector((state) => state.site);
 	const words = useWords();
 
 	let items;
 
+	if (social.length !== 0) {
+		items = React.Children.toArray(
+			social.map((item) => {
+				let prefix = "";
+
+				if (item.name === "Twitter") prefix = "https://twitter.com/";
+				else if (item.name === "Github") prefix = "https://github.com/";
+				else if (item.name === "LinkedIn")
+					prefix = "https://www.linkedin.com/in/";
+				else {
+					if (!item.link.includes("https://") && !item.link.includes("http://"))
+						prefix = "https://";
+					else prefix = "";
+				}
+
+				return (
+					<>
+						<div className={styles.item}>
+							<strong>{item.name}</strong>
+						</div>
+						<div>
+							<a href={prefix + item.link} target="_blank">
+								{prefix !== "https://" && prefix !== "http://" && prefix !== ""
+									? "@"
+									: ""}
+								{item.link}
+							</a>
+						</div>
+					</>
+				);
+			})
+		);
+	}
+
 	return (
 		<div>
-			
-		</div>   
-    )
+
+		</div>
+	)
 }
 
 export default PreviewSocial;
