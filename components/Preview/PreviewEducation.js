432579d2f9a06d989e9ebecd979e56cb70673560
MelakuDemeke <melaku.demeke789@gmail.com>
2023-08-30 16:32:20 +0300

    preview style done

--- components/Preview/PreviewEducation.js
+++ components/Preview/PreviewEducation.js
@@ -3,9 +3,45 @@ import { useSelector } from "react-redux";
 import { useWords } from "@/hooks";
 
 const PreviewEducation = () => {
-	return (
-		<div>
+	const { education } = useSelector((state) => state.form);
+	const { colors } = useSelector((state) => state.site);
+	const words = useWords();
 
+	let items;
+
+	if (education.length !== 0) {
+		items = React.Children.toArray(
+			education.map((item) => (
+				<div className="flex flex-col mb-4">
+					<div className="flex flex-wrap justify-between">
+						<strong>
+							{item.degree} / {item.subject}
+						</strong>
+						<span>
+							{item.from} - {item.to}
+						</span>
+					</div>
+					<div>
+						{item.school}, {item.city}
+					</div>
+				</div>
+			))
+		);
+	}
+
+	return (
+		<div className="flex flex-col w-full px-6 mt-2">
+			{education.length !== 0 && (
+				<>
+					<div
+						className="p-2 mb-4 mt-4 font-bold text-center tracking-widest"
+						style={{ background: colors.primary, color: colors.titles }}
+					>
+						{words.education_upper}
+					</div>
+					{items}
+				</>
+			)}
 		</div>
 	)
 }
