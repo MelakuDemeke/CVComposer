88a41b892438d27eb6673cd2733ab0b68e373a6d
MelakuDemeke <melaku.demeke789@gmail.com>
2023-08-30 15:31:11 +0300

    import required components and store

--- components/Form/FormHobbies.js
+++ components/Form/FormHobbies.js
@@ -1,8 +1,19 @@
 import Card from "../Card/Card";
+import { useDispatch, useSelector } from "react-redux";
+import { setHobbies } from "@/app/GlobalRedux/form";
+import { nanoid } from "nanoid";
+import CardDetail from "../FormComponents/CardDetail";
+import { Buttons } from "../FormComponents/Buttons";
 
 const FormHobbies = ({ handleResetData }) => {
+	const dispatch = useDispatch();
+  const { hobbies } = useSelector((state) => state.form);
+  const words = useWords();
+
 	return (
-		<Card title="Hobbies"></Card>
+		<Card title={words.hobbies}>
+			
+		</Card>
 	)
 }
 
