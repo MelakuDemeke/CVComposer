80a4b40bc3b11003ea65ee7346d2b02f8146dac4
MelakuDemeke <melaku.demeke789@gmail.com>
2023-08-30 10:53:16 +0300

    component render for review with drag and drop

--- components/Preview/Preview.js
+++ components/Preview/Preview.js
@@ -5,6 +5,7 @@ import PreviewPersonal from "./PreviewPersonal";
 import { setLeftSide, setRightSide } from "@/app/GlobalRedux/site";
 import React, { forwardRef } from "react";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import PreviewSocial from "./PreviewSocial";
 
 const Preview = forwardRef(({ array, ref }) => {
 	const dispatch = useDispatch();
@@ -27,10 +28,47 @@ const Preview = forwardRef(({ array, ref }) => {
 		items.splice(result.destination.index, 0, reorderedItem);
 		dispatch(setRightSide(items));
 	};
+
+	const previews = {
+		PreviewSocial: <PreviewSocial />
+	}
+
 	return (
-		<div className="w-full h-full flex items-start justify-start overflow-auto">
+		<div className="w-full h-full flex items-start justify-start overflow-auto"
+			style={{ background: colors.body }}
+			ref={ref}
+			contentEditable={isContentEditable}
+			suppressContentEditableWarning={true}
+		>
 			<div style={{ background: colors.secondary, color: colors.text1 }} className="w-2/6 h-auto min-h-full p-6">
 				<PreviewPersonal />
+				<DragDropContext onDragEnd={handleOnDrugEndLeft}>
+					<Droppable droppableId="leftSide">
+						{(provided) => (
+							<div {...provided.droppableProps} ref={provided.innerRef}>
+								{leftSide.map((item, index) => (
+									<Draggable
+										key={`${item.id}`}
+										draggableId={`${item.id}`}
+										index={index}
+										isDragDisabled={isContentEditable}
+									>
+										{(provided) => (
+											<div
+												{...provided.draggableProps}
+												{...provided.dragHandleProps}
+												ref={provided.innerRef}
+											>
+												{previews[item.component]}
+											</div>
+										)}
+									</Draggable>
+								))}
+								{provided.placeholder}
+							</div>
+						)}
+					</Droppable>
+				</DragDropContext>
 			</div>
 			<div style={{ color: colors.text2 }} className="w-4/6 h-auto min-h-full">
 				<PreviewHeader />
