94ec3b20c620dee73c1ed4b8fae6416884aaf8fe
MelakuDemeke <melaku.demeke789@gmail.com>
2023-09-01 16:52:08 +0300

    tittle added

--- components/LandingPage/Features.js
+++ components/LandingPage/Features.js
@@ -1,10 +1,13 @@
 import { MdPhoneIphone } from "react-icons/md";
-import { BsPalette2,BsFiletypePdf } from "react-icons/bs";
-import {MdSupportAgent} from "react-icons/md";
+import { BsPalette2, BsFiletypePdf } from "react-icons/bs";
+import { MdSupportAgent } from "react-icons/md";
 
 const Features = () => {
 	return (
 		<div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+			<div class="max-w-2xl mx-auto text-center mb-10 lg:mb-14">
+				<h2 class="text-2xl font-bold md:text-4xl md:leading-tight dark:text-white">Our Features</h2>
+			</div>
 			<div className="grid sm:grid-cols-2 lg:grid-cols-4 items-center gap-12">
 				<div>
 					<MdPhoneIphone className="w-9 h-9 text-gray-800 dark:text-white" />
@@ -17,7 +20,7 @@ const Features = () => {
 					</div>
 				</div>
 				<div>
-					<BsPalette2  className="w-9 h-9 text-gray-800 dark:text-white"/>
+					<BsPalette2 className="w-9 h-9 text-gray-800 dark:text-white" />
 					<div className="bg-gradient-to-r from-gray-200 to-white/0 h-0.5 mt-6 dark:from-gray-700 dark:to-slate-900/0">
 						<div className="bg-gray-400 w-9 h-0.5"></div>
 					</div>
@@ -27,7 +30,7 @@ const Features = () => {
 					</div>
 				</div>
 				<div>
-					<BsFiletypePdf className="w-9 h-9 text-gray-800 dark:text-white"/>
+					<BsFiletypePdf className="w-9 h-9 text-gray-800 dark:text-white" />
 					<div className="bg-gradient-to-r from-gray-200 to-white/0 h-0.5 mt-6 dark:from-gray-700 dark:to-slate-900/0">
 						<div className="bg-gray-400 w-9 h-0.5"></div>
 					</div>
@@ -37,7 +40,7 @@ const Features = () => {
 					</div>
 				</div>
 				<div>
-					<MdSupportAgent className="w-9 h-9 text-gray-800 dark:text-white"/>
+					<MdSupportAgent className="w-9 h-9 text-gray-800 dark:text-white" />
 					<div className="bg-gradient-to-r from-gray-200 to-white/0 h-0.5 mt-6 dark:from-gray-700 dark:to-slate-900/0">
 						<div className="bg-gray-400 w-9 h-0.5"></div>
 					</div>
