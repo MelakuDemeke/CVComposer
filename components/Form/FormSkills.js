a7c00ae56d3fb8a9c1849b83c02fd95e597fabb4
MelakuDemeke <melaku.demeke789@gmail.com>
2023-09-01 11:31:39 +0300

    imort necessary library and component

--- components/Form/FormSkills.js
+++ components/Form/FormSkills.js
@@ -1,4 +1,12 @@
-const { default: Card } = require("../Card/Card")
+import Card from "../Card/Card";
+import { Formik, Form } from "formik"
+import { Input, Select} from "../FormComponents"
+import { useDispatch, useSelector } from "react-redux";
+import { setSkills } from "@/app/GlobalRedux/form";
+import { nanoid } from "nanoid";
+import CardDetail from "../FormComponents/CardDetail";
+import { Buttons } from "../FormComponents/Buttons";
+import { useWords } from "@/hooks";
 
 const FormSkills = () => {
 	return (
