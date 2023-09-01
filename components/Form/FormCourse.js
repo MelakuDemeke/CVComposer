d75e4218ce2a2071ad4b0fd56d4ea0ce2d4950b7
MelakuDemeke <melaku.demeke789@gmail.com>
2023-09-01 12:06:30 +0300

    import component and lib

--- components/Form/FormCourse.js
+++ components/Form/FormCourse.js
@@ -1,4 +1,12 @@
 const { default: Card } = require("../Card/Card")
+import { Formik, Form } from "formik"
+import { Input, Textarea } from "../FormComponents"
+import { useDispatch, useSelector } from "react-redux"
+import { setCourses } from "@/app/GlobalRedux/form"
+import nanoid from "nanoid"
+import CardDetail from "../FormComponents/CardDetail"
+import { Buttons } from "../FormComponents/Buttons"
+
 
 const FormCourse = () => {
 	return (
