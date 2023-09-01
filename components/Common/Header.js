62e8ba546b153fe12ef3e14b4a274b7ae8a6ba1f
MelakuDemeke <melaku.demeke789@gmail.com>
2023-09-01 15:28:19 +0300

    svg replaced by reacticon

--- components/Common/Header.js
+++ components/Common/Header.js
@@ -4,6 +4,8 @@ import ThemeSwitcher from "@/app/ThemeSwitcher";
 import Link from "next/link";
 import { useState } from "react";
 import { usePathname } from 'next/navigation'
+import { RxHamburgerMenu } from "react-icons/rx";
+import { LiaTimesSolid } from "react-icons/lia";
 
 const Header = () => {
 	const [openMenu, setOpenMenu] = useState(false);
@@ -22,11 +24,7 @@ const Header = () => {
 					</a>
 					<div className="sm:hidden">
 						<button onClick={handleMenuToggle} className={` hs-collapse-toggle p-2 inline-flex justify-center items-center gap-2 rounded-md border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm dark:bg-slate-900 dark:hover:bg-slate-800 dark:border-gray-700 dark:text-gray-400 dark:hover:text-white dark:focus:ring-offset-gray-800`} data-hs-collapse="#navbar-collapse-with-animation" aria-controls="navbar-collapse-with-animation" aria-label="Toggle navigation">
-							{openMenu ? (<svg className="hs-collapse-open:hidden w-4 h-4" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-								<path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
-							</svg>) : (<svg className="hs-collapse-open:hidden w-4 h-4" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-								<path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
-							</svg>)}
+							{openMenu ? (<LiaTimesSolid />) : (<RxHamburgerMenu/>)}
 						</button>
 					</div>
 				</div>
@@ -35,6 +33,7 @@ const Header = () => {
 						<Link className={`${currentRoute === '/' ? "text-blue-600 dark:text-blue-500" : "text-gray-500 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500"} font-medium  sm:py-6 `} href="/" aria-current="page">Home</Link>
 						<Link className={`${currentRoute === '/' ? "text-blue-600 dark:text-blue-500" : "text-gray-500 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500"} font-medium  sm:py-6 `} href="/cvbuilder" aria-current="page">Create CV</Link>
 						<ThemeSwitcher />
+						
 					</div>
 				</div>
 			</nav>
