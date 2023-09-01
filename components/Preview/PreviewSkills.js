d4b8fb450d81a30852e3823003985594759bade2
MelakuDemeke <melaku.demeke789@gmail.com>
2023-09-01 11:52:04 +0300

    skill preview built

--- components/Preview/PreviewSkills.js
+++ components/Preview/PreviewSkills.js
@@ -1,12 +1,50 @@
+import React from "react";
 import { useSelector } from "react-redux";
 import { FaCircle, FaRegCircle } from "react-icons/fa";
 import { useWords } from "@/hooks";
 
 const PreviewSkills = () => {
-    return (
-        <div>
+	const { skills } = useSelector((state) => state.form);
+	const { colors } = useSelector((state) => state.site);
+	const words = useWords();
 
-        </div>
-    )
+	let items;
+
+	if (skills.length !== 0) {
+		items = React.Children.toArray(
+			skills.map((item) => {
+				const levels = [];
+				for (let index = 1; index <= item.level; index++) {
+					levels.push(React.Children.toArray(<FaCircle />));
+				}
+				for (let index = item.level; index < 10; index++) {
+					levels.push(React.Children.toArray(<FaRegCircle />));
+				}
+				return (
+					<div className="flex flex-col mb-3">
+						<div className="flex flex-wrap justify-between items-center">
+							<strong>{item.name}</strong>
+							<span className="flex gap-x-[2px]">{levels}</span>
+						</div>
+					</div>
+				);
+			})
+		);
+	}
+	return (
+		<div className="flex flex-col w-full px-6 mt-2">
+			{skills.length !== 0 && (
+        <>
+          <div
+            className="p-2 mb-4 mt-4 font-bold text-center tracking-widest"
+            style={{ background: colors.primary, color: colors.titles }}
+          >
+            {words.skills_upper}
+          </div>
+          {items}
+        </>
+      )}
+		</div>
+	)
 }
 export default PreviewSkills;
