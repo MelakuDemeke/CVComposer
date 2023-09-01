ba602e9e22c0584a86870af1216803316a698e37
MelakuDemeke <melaku.demeke789@gmail.com>
2023-09-01 15:53:25 +0300

    replace phone svg with md icon

--- components/LandingPage/Features.js
+++ components/LandingPage/Features.js
@@ -1,12 +1,13 @@
+import { MdPhoneIphone } from "react-icons/md";
+
 const Features = () => {
 	return (
 		<div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
 			<div className="grid sm:grid-cols-2 lg:grid-cols-4 items-center gap-12">
 				<div>
-					<svg className="w-9 h-9 text-gray-800 dark:text-white" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-						<path d="M11 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h6zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H5z" />
-						<path d="M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
-					</svg>
+
+					<MdPhoneIphone className="w-9 h-9 text-gray-800 dark:text-white" />
+					
 					<div className="bg-gradient-to-r from-gray-200 to-white/0 h-0.5 mt-6 dark:from-gray-700 dark:to-slate-900/0">
 						<div className="bg-gray-400 w-9 h-0.5"></div>
 					</div>
