3cbd38e8573f90fe93abc8889c7db5597491fa89
MelakuDemeke <melaku.demeke789@gmail.com>
2023-09-04 16:49:21 +0300

    about page template

---
+++ components/LandingPage/About.js
@@ -0,0 +1,12 @@
+
+const About = () => {
+	return (
+		<div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+			<div className="max-w-2xl mx-auto text-center mb-10 lg:mb-14">
+				<h2 className="text-2xl font-bold md:text-4xl md:leading-tight dark:text-white">About Us</h2>
+			</div>
+		</div>
+	);
+}
+
+export default About;
