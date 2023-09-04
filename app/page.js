4910416372cbe8ef281c58462c77647d6e6b4ae3
MelakuDemeke <melaku.demeke789@gmail.com>
2023-09-04 12:30:55 +0300

    renderd features detail section

--- app/page.js
+++ app/page.js
@@ -3,12 +3,14 @@ import Hero from '@/components/LandingPage/Hero'
 import Stats from '@/components/LandingPage/Stats'
 import Testimonials from '@/components/LandingPage/Testimonials'
 import Team from '@/components/LandingPage/Team'
+import FeaturesDetail from '@/components/LandingPage/FeaturesDetail'
 
 export default function Home() {
   return (
     <main className="min-h-screen">
       <Hero />
       <Stats />
+      <FeaturesDetail />
       <Team />
       <Testimonials />
       <Features />
