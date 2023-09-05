f9d9a3614d5d3f1e0e546acdceeb09b7f0702e90
MelakuDemeke <melaku.demeke789@gmail.com>
2023-09-05 17:01:40 +0300

    languge basic template added

---
+++ components/Lang/Language.js
@@ -0,0 +1,13 @@
+import { useDispatch, useSelector } from "react-redux";
+import { setLanguage } from "@/app/GlobalRedux/language";
+import ReactCountryFlag from "react-country-flag";
+
+const Language = () => {
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default Language;
