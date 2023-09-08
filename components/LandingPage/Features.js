77accdcbd5be0e8727fe23df7f0cdae0a2cf58f7
MelakuDemeke <melaku.demeke789@gmail.com>
2023-09-08 13:52:19 +0300

    minor spelling error fix

--- components/LandingPage/Features.js
+++ components/LandingPage/Features.js
@@ -6,7 +6,7 @@ const Features = () => {
 	return (
 		<div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
 			<div class="max-w-2xl mx-auto text-center mb-10 lg:mb-14">
-				<h2 class="text-2xl font-bold md:text-4xl md:leading-tight dark:text-white">Why Chose Us</h2>
+				<h2 class="text-2xl font-bold md:text-4xl md:leading-tight dark:text-white">Why Choose Us</h2>
 			</div>
 			<div className="grid sm:grid-cols-2 lg:grid-cols-4 items-center gap-12">
 				<div>
