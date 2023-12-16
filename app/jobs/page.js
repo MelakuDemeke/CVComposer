da89ae3ce4e6541f8f49fae73119c30f85fb5ac4
MelakuDemeke <melaku.demeke789@gmail.com>
2023-12-16 11:37:47 +0300

    add jobs route

---
+++ app/jobs/page.js
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Home() {
+	return (
+		<div className="min-h-screen h-auto lg:h-screen flex flex-wrap  overflow-y-auto">
+			
+		</div>
+	)
+}
--- components/Common/Header.js
+++ components/Common/Header.js
@@ -32,6 +32,7 @@ const Header = () => {
 					<div className="flex flex-col gap-y-4 gap-x-0 mt-5 sm:flex-row sm:items-center sm:justify-end sm:gap-y-0 sm:gap-x-7 sm:mt-0 sm:pl-7">
 						<Link className={`${currentRoute === '/' ? "text-blue-600 dark:text-blue-500" : "text-gray-500 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500"} font-medium  sm:py-6 `} href="/" aria-current="page">Home</Link>
 						<Link className={`${currentRoute === '/cvbuilder' ? "text-blue-600 dark:text-blue-500" : "text-gray-500 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500"} font-medium  sm:py-6 `} href="/cvbuilder" aria-current="page">Create CV</Link>
+						<Link className={`${currentRoute === '/jobs' ? "text-blue-600 dark:text-blue-500" : "text-gray-500 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500"} font-medium  sm:py-6 `} href="/jobs" aria-current="page">Jobs</Link>
 						<ThemeSwitcher />
 						
 					</div>
